201efaab9564da9d31dfe0dae92e958ace3b75c6
CooperLi1 <112364250+CooperLi1@users.noreply.github.com>
2025-05-28 23:06:50 -0400

    mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function MediaPage() {
   return (
-    <div className="flex h-screen p-4 bg-gray-100">
-      <div className="w-1/2 h-full pr-2">
+    <div className="flex flex-col md:flex-row h-screen p-4 bg-gray-100">
+      {/* Left Image: visible only on md and up */}
+      <div className="hidden md:block w-full md:w-1/2 h-64 md:h-full pr-0 md:pr-2">
         <div className="w-full h-full rounded-2xl overflow-hidden shadow-lg">
           <img
             src="/largerminime.png"
@@ -11,18 +12,21 @@ export default function MediaPage() {
         </div>
       </div>
 
-      <div className="w-1/2 h-full pl-2 flex flex-col">
-        <div className="h-1/2 mb-2 rounded-2xl overflow-hidden shadow-lg bg-white">
+      {/* Right Side: slides + video */}
+      <div className="w-full md:w-1/2 h-full flex flex-col space-y-4 md:pl-2">
+        {/* Slides */}
+        <div className="flex-1 rounded-2xl overflow-hidden shadow-lg bg-white">
           <iframe
             src="https://docs.google.com/presentation/d/1u2F9dbSZzd14WiYGAZrbvXYlgR2EtQkKTPUDzdTm9iI/embed"
             frameBorder="0"
             width="100%"
-            height="100%" 
+            height="100%"
             allowFullScreen
           ></iframe>
         </div>
 
-        <div className="h-1/2 rounded-2xl overflow-hidden shadow-lg bg-white">
+        {/* Video */}
+        <div className="flex-1 rounded-2xl overflow-hidden shadow-lg bg-white">
           <iframe
             src="https://www.youtube.com/embed/F8KEMP-Q4tQ"
             title="YouTube video"
